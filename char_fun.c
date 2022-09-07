431ba853caac1183be7b3afd3a0dee5047c6230f
ZeharaY <106754679+zeharaY@users.noreply.github.com>
2022-09-07 18:23:14 +0300

    com

---
+++ char_fun.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * _putchar - writes the character to stdout
+ * @c: character to print
+ *
+ * Return: 1 or -1 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _strncpy - copie a string
+ * @dest:char
+ *  @src:char
+ * @n:int
+ * Return:char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+i = 0;
+	while (i < n && *(src + i))
+	{
+	*(dest + i) = *(src + i);
+	i++;
+	}
+	while (i < n)
+	{
+	*(dest + i) = '\0';
+	i++;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+
+/**
+ * _atoi - convert to int
+ * @s:string
+ * Return:int
+ */
+int _atoi(char *s)
+{
+int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}
+/**
+ * _puts - print a string
+ * @str:pointer char
+ * return:void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
+return;
+}
